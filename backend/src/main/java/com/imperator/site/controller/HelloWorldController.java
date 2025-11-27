4e1de1d9e66094355ce6b630d22bc2228fbc2dc6
QuodImperator <darkostefanoski951@gmail.com>
2025-11-27 18:15:32 +0100

    Updated project structure and basic testing

---
+++ backend/src/main/java/com/imperator/site/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.imperator.site.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = {"", "/", "/home"})
+    public String helloWorld(){
+        return "Hello world!!";
+    }
+}
